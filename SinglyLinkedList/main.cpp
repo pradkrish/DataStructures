c32803e3d6ea9d1affaa0b9b5dc332f69a801df2
pradeep <pradeep.krishnamurthy@excillum.com>
2019-02-02 17:05:23 +0100

    Separated the linked list definitions from declarations

--- SinglyLinkedList/main.cpp
+++ SinglyLinkedList/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
-#include "Node.hpp"
-#include "LinkedList.hpp"
+#include "LinkedList.cpp"
 
 int main()
 {
@@ -8,14 +7,13 @@ int main()
   LinkedList<int> linkedList = LinkedList<int>();
   
   linkedList.InsertHead(43);
-
   linkedList.InsertHead(76);
   linkedList.InsertTail(15);
   linkedList.InsertTail(44);
   linkedList.InsertTail(100);
   linkedList.Insert(3, 48);
-  linkedList.PrintList();
+  linkedList.PrintList(); // 76->43->15->48->44->100->NULL
   linkedList.Remove(2);
-  linkedList.PrintList();
+  linkedList.PrintList(); // 76->43->48->44->100->NULL
   return 0;
 }
